b534e1ac5b4ceeade12141b31480129d30e7e6fc
Pete Jabbour <pete@learnmetrics.com>
2015-09-14 11:35:34 -0500

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Gmail mailer configuration
   config.action_mailer.delivery_method = :letter_opener
   
-  # # Don't care if the mailer can't send
+  # Don't care if the mailer can't send
   # config.action_mailer.raise_delivery_errors = false
   # 
   # # Send out mailers when .delivery is invoked
@@ -13,14 +13,14 @@ Rails.application.configure do
   # # Gmail mailer configuration
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
-  #   :address              => 'smtp.gmail.net',
+  #   :address              => 'smtp.gmail.com',
   #   :port                 => 587,
   #   :domain               => 'petejabbour.com',
   #   :user_name            => ENV['GMAIL_USERNAME'],
   #   :password             => ENV['GMAIL_PASSWORD'],
   #   :authentication       => 'plain',
   #   :enable_starttls_auto => true  }
-    
+
   # Set the default host option for mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Gmail mailer configuration
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.net',
+    :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'www.petejabbour.com',
     :user_name            => ENV['GMAIL_USERNAME'],
