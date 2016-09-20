749a1b78ee3a5c6781aff379f0acc300e5891dd1
Pete Jabbour <petejabbour1@gmail.com>
2016-09-20 11:45:42 -0500

    more

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "Pete Jabbour's Website <petejabbour1website@gmail.com>"
+  default from: "Pete Jabbour's Website <postmaster@petejabbour.info>"
   default content_type: "text/html"
-  
+
   def contact_request_email(contact_request)
     @contact_request = contact_request
     mail(to: "Pete Jabbour <petejabbour1@gmail.com>", subject: "New Contact Request")
   end
-  
+
 end
