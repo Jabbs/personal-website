ac4cdaf5b0ec8b84e0d06f35735d10f3a0f802b3
Pete Jabbour <petejabbour1@gmail.com>
2018-11-16 08:42:52 -0600

    small changes; added require 'timeout'

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "Pete Jabbour's Website <postmaster@petejabbour.info>"
+  default from: "Peter Jabbour's Website <postmaster@petejabbour.info>"
   default content_type: "text/html"
 
   def contact_request_email(contact_request)
     @contact_request = contact_request
-    mail(to: "Pete Jabbour <petejabbour1@gmail.com>", subject: "New Contact Request")
+    mail(to: "Peter Jabbour <petejabbour1@gmail.com>", subject: "New Contact Request")
   end
 
 end
