db2f1f12312f2a159bf78731f32aa162e8b80d07
Peter Jabbour <pjabbour@urbanbound.com>
2019-11-11 09:16:23 -0600

    rest of postgres upgrade post

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   match '/contact', to: "static_pages#contact", via: :get
   match '/services', to: "static_pages#services", via: :get
   match '/portfolio', to: "static_pages#portfolio", via: :get
-  match '/blog/11-10-2019/postgres-upgrade/9.4/to/9.6/psql', to: "static_pages#postgres_upgrade", via: :get, as: :postgres_blog
+  match '/blog/11-11-2019/9.4/to/9.6/postgres-upgrade', to: "static_pages#postgres_upgrade", via: :get, as: :postgres_blog
 end
