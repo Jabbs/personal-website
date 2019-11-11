aac158b7d50b01cf683332813bfb798b7282b875
Peter Jabbour <pjabbour@urbanbound.com>
2019-11-11 09:50:05 -0600

    moved google key to env var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_google_maps_key
-
-  def set_google_maps_key
-    @google_maps_key = "AIzaSyDtB2EUXSb38_3HcXeNZtfyi-7gFd2Kpk0"
-  end
 end
